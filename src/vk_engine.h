31bd2fe930c8d5a02dfe2d0a1e020a6a5b587e8f
i.shapturov <i.shapturov@corp.mail.ru>
2023-09-03 13:43:10 +0300

    + render loop

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -4,9 +4,37 @@
 #pragma once
 
 #include <vk_types.h>
+#include <vector>
 
 class VulkanEngine {
 public:
+
+	VkInstance _instance; // Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU; // GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface; // Vulkan window surface
+
+	VkSwapchainKHR _swapchain; // from other articles
+
+	// image format expected by the windowing system
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages = {};
+	std::vector<VkImageView> _swapchainImageViews = {};
+
+	VkQueue _graphicsQueue; //queue we will submit to
+	uint32_t _graphicsQueueFamily; //family of that queue
+
+	VkCommandPool _commandPool; //the command pool for our commands
+	VkCommandBuffer _mainCommandBuffer; //the buffer we will record into
+
+	VkRenderPass _renderPass;
+
+	std::vector<VkFramebuffer> _framebuffers;
+
+	VkSemaphore _presentSemaphore, _renderSemaphore;
+	VkFence _renderFence;
 
 	bool _isInitialized{ false };
 	int _frameNumber {0};
@@ -26,4 +54,18 @@ public:
 
 	//run main loop
 	void run();
+
+private:
+
+	void init_vulkan();
+
+	void init_swapchain();
+
+	void init_commands();
+
+	void init_default_renderpass();
+
+	void init_framebuffers();
+
+	void init_sync_structures();
 };
