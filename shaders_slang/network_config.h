df5adb85e7c4d26010feab282ae6b45aab1bf9fa
i.shapturov <iulian.shapturov@my.games>
2025-07-19 14:47:02 +0300

    + optimize nrc

--- shaders_slang/network_config.h
+++ shaders_slang/network_config.h
@@ -5,8 +5,8 @@
 #define OUTPUT_NEURONS 4
 
 
-#define HIDDEN_NEURONS 64
-#define NUM_HIDDEN_LAYERS 7
+#define HIDDEN_NEURONS 32
+#define NUM_HIDDEN_LAYERS 3
 #define BATCH_SIZE (1 << 16)
 #define BATCH_COUNT 1
 
